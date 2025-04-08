f7587ec7dfb6c9e41d7588bf2bc600edff02ce5a
sersim33 <sersim33@gmail.com>
2025-04-08 20:03:25 +0300

    Updated DAG to use full GitHub paths

--- Sergii_S/project_solution.py
+++ Sergii_S/project_solution.py
@@ -1,5 +1,5 @@
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime
 import os
 
@@ -12,7 +12,7 @@ default_args = {
 # Connection ID for Spark
 connection_id = 'spark-default'
 
-# Define DAG
+# Define the DAG
 with DAG(
         'FP_Sergii_S',
         default_args=default_args,
@@ -21,15 +21,10 @@ with DAG(
         tags=["Sergii_S"]
 ) as dag:
 
-    # Define the full path to the scripts in the GitHub folder
-    landing_to_bronze_path = '/opt/airflow/airflow_sandbox/Sergii_S/landing_to_bronze.py'
-    bronze_to_silver_path = '/opt/airflow/airflow_sandbox/Sergii_S/bronze_to_silver.py'
-    silver_to_gold_path = '/opt/airflow/airflow_sandbox/Sergii_S/silver_to_gold.py'
-
-    # Define SparkSubmitOperator tasks with full paths to the scripts
+    # Define SparkSubmitOperator tasks with relative paths
     landing_to_bronze = SparkSubmitOperator(
         task_id='landing_to_bronze',
-        application=landing_to_bronze_path,  # Full path
+        application='Sergii_S/landing_to_bronze.py',  # relative path
         conn_id=connection_id,
         verbose=1,
         dag=dag,  # Associate the task with the current DAG
@@ -37,7 +32,7 @@ with DAG(
 
     bronze_to_silver = SparkSubmitOperator(
         task_id='bronze_to_silver',
-        application=bronze_to_silver_path,  # Full path
+        application='Sergii_S/bronze_to_silver.py',  # relative path
         conn_id=connection_id,
         verbose=1,
         dag=dag,  # Associate the task with the current DAG
@@ -45,7 +40,7 @@ with DAG(
 
     silver_to_gold = SparkSubmitOperator(
         task_id='silver_to_gold',
-        application=silver_to_gold_path,  # Full path
+        application='Sergii_S/silver_to_gold.py',  # relative path
         conn_id=connection_id,
         verbose=1,
         dag=dag,  # Associate the task with the current DAG
@@ -53,4 +48,5 @@ with DAG(
 
     # Define task dependencies
     landing_to_bronze >> bronze_to_silver >> silver_to_gold
+
 
