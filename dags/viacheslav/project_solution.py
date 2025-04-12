1d3d3f8e36bd3128343ede13a567324c33eb3089
samatwa <samatwa@gmail.com>
2025-04-12 17:19:27 +0200

    changes

--- dags/viacheslav/project_solution.py
+++ dags/viacheslav/project_solution.py
@@ -2,33 +2,30 @@ from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime, timedelta
 
-# Define default arguments
 default_args = {
     'owner': 'airflow',
-    'retry_delay': timedelta(minutes=5),
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1
 }
 
-# Create DAG
 dag = DAG(
     'athlete_data_pipeline_viach',
     default_args=default_args,
     description='Data pipeline for athlete data processing',
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
     catchup=False,
 )
 
-# Define tasks using SparkSubmitOperator
 landing_to_bronze_task = SparkSubmitOperator(
-    application='/home/samatwa77/airflow/dags/viacheslav/landing_to_bronze_viach.py',
+    application='dags/viacheslav/landing_to_bronze.py',
     task_id='landing_to_bronze',
-    conn_id='spark-default',  # Using the default Spark connection
+    conn_id='spark-default',
     verbose=1,
     dag=dag,
 )
 
 bronze_to_silver_task = SparkSubmitOperator(
-    application='/home/samatwa77/airflow/dags/viacheslav/bronze_to_silver_viach.py',
+    application='dags/viacheslav/bronze_to_silver.py',
     task_id='bronze_to_silver',
     conn_id='spark-default',
     verbose=1,
@@ -36,12 +33,11 @@ bronze_to_silver_task = SparkSubmitOperator(
 )
 
 silver_to_gold_task = SparkSubmitOperator(
-    application='/home/samatwa77/airflow/dags/viacheslav/silver_to_gold_viach.py', 
+    application='viacheslav/silver_to_gold.py',
     task_id='silver_to_gold',
     conn_id='spark-default',
     verbose=1,
     dag=dag,
 )
 
-# Set task dependencies
 landing_to_bronze_task >> bronze_to_silver_task >> silver_to_gold_task
