d1b53ba7b26a3cb97f3094522b5388a6bdc7e4f1
Olesia Kholod <olesiakholod@MacBook-Air-Olesia.local>
2025-05-08 12:09:22 +0300

    Fix DAG script paths for final project

--- dags/Olesia/project_solution.py
+++ dags/Olesia/project_solution.py
@@ -7,7 +7,7 @@ dags_dir = os.path.dirname(os.path.realpath(__file__))
 
 default_args = {
     'start_date': datetime(2025, 5, 7),
-    'owner': 'olesia',
+    'owner': 'olesia'
 }
 
 with DAG(
@@ -21,23 +21,23 @@ with DAG(
 
     landing_to_bronze = SparkSubmitOperator(
         task_id="landing_to_bronze",
-        application=os.path.join(dags_dir, "Olesia", "landing_to_bronze.py"),
+        application=os.path.join(dags_dir, "landing_to_bronze.py"),
         conn_id="spark-default",
-        verbose=True,
+        verbose=True
     )
 
     bronze_to_silver = SparkSubmitOperator(
         task_id="bronze_to_silver",
-        application=os.path.join(dags_dir, "Olesia", "bronze_to_silver.py"),
+        application=os.path.join(dags_dir, "bronze_to_silver.py"),
         conn_id="spark-default",
-        verbose=True,
+        verbose=True
     )
 
     silver_to_gold = SparkSubmitOperator(
         task_id="silver_to_gold",
-        application=os.path.join(dags_dir, "Olesia", "silver_to_gold.py"),
+        application=os.path.join(dags_dir, "silver_to_gold.py"),
         conn_id="spark-default",
-        verbose=True,
+        verbose=True
     )
 
     landing_to_bronze >> bronze_to_silver >> silver_to_gold
