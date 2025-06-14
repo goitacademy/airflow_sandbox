166a55bdbcb79e315a7ba6c80b40f2075ced2501
ili_Nandr_ii <lustfulwolf@gmail.com>
2025-06-14 21:24:30 +0200

    Encrypt project_solution.py for ilin v2

--- dags/ilin/project_solution.py
+++ dags/ilin/project_solution.py
@@ -12,21 +12,21 @@ with DAG(
     landing_to_bronze = SparkSubmitOperator(
         task_id="landing_to_bronze",
         application="dags/ilin/landing_to_bronze.py",
-        conn_id="spark-default",
+        conn_id="mysql_ilin",
         verbose=1
     )
 
     bronze_to_silver = SparkSubmitOperator(
         task_id="bronze_to_silver",
         application="dags/ilin/bronze_to_silver.py",
-        conn_id="spark-default",
+        conn_id="mysql_ilin",
         verbose=1
     )
 
     silver_to_gold = SparkSubmitOperator(
         task_id="silver_to_gold",
         application="dags/ilin/silver_to_gold.py",
-        conn_id="spark-default",
+        conn_id="mysql_ilin",
         verbose=1
     )
 
