a4f2c7d95ac3783397243c4081506e3c85e53ed9
MMatviiuk <m.v.matviiuk@gmail.com>
2025-06-27 16:08:38 +0300

    Part 2 final

--- mm_project_solution.py
+++ mm_project_solution.py
@@ -8,7 +8,7 @@ dags_dir = os.path.dirname(os.path.realpath(__file__))
 
 default_args = {
     "owner": "airflow",
-    "start_date": datetime(2025, 4, 12),
+    "start_date": datetime(2025, 6, 26),
     "depends_on_past": False,
     "retries": 1,
 }
@@ -23,21 +23,21 @@ with DAG(
 
     landing_to_bronze = SparkSubmitOperator(
         task_id="mm_landing_to_bronze",
-        application=os.path.join(dags_dir, "landing_to_bronze.py"),
+        application=os.path.join(dags_dir, "mm_landing_to_bronze.py"),
         conn_id="spark-default",
         verbose=True,
     )
 
     bronze_to_silver = SparkSubmitOperator(
         task_id="mm_bronze_to_silver",
-        application=os.path.join(dags_dir, "bronze_to_silver.py"),
+        application=os.path.join(dags_dir, "mm_bronze_to_silver.py"),
         conn_id="spark-default",
         verbose=True,
     )
 
     silver_to_gold = SparkSubmitOperator(
         task_id="mm_silver_to_gold",
-        application=os.path.join(dags_dir, "silver_to_gold.py"),
+        application=os.path.join(dags_dir, "mm_silver_to_gold.py"),
         conn_id="spark-default",
         verbose=True,
     )
