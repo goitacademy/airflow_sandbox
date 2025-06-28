be392e8e1f3a40a1334dd75dcddd703e61824e30
OF3000 <ofpost@i.ua>
2025-06-28 14:05:08 +0300

    Шифрування файлів та додавання до репозиторію.

--- ofHW7.py
+++ ofHW7.py
@@ -1,7 +1,7 @@
 from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator, BranchPythonOperator
-from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-from airflow.providers.common.sql.sensors.sql import SqlSensor
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.sensors.sql import SqlSensor
 from datetime import datetime, timedelta
 import random
 import time
