62ade6aded61b49515310b45d9a74367f61f1c87
OF3000 <ofpost@i.ua>
2025-06-28 16:21:05 +0300

    Шифрування файлів та додавання до репозиторію.

--- ofHW7.py
+++ ofHW7.py
@@ -9,21 +9,20 @@ import time
 default_args = {
     'owner': 'airflow',
     'retries': 1,
-    'retry_delay': timedelta(seconds=10),
+    'retry_delay': timedelta(seconds=10)
 }
 
 with DAG(
     dag_id='of_medal_count_pipeline',
     default_args=default_args,
-    start_date=datetime(2024, 1, 1),
-    schedule=None,  # без розкладу, запускається вручну
+    start_date=datetime(2023, 1, 1),
+    schedule_interval=None,
     catchup=False
 ) as dag:
 
-    # Створення таблиці
-    create_table = SQLExecuteQueryOperator(
+    create_table = MySqlOperator(
         task_id='create_table',
-        conn_id='mysql_default',
+        mysql_conn_id='mysql_default',
         sql="""
         CREATE TABLE IF NOT EXISTS neo_data.of_medals_results (
             id INT AUTO_INCREMENT PRIMARY KEY,
@@ -31,11 +30,10 @@ with DAG(
             count INT,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );
-        """,
-        hook_params={'schema': 'neo_data'}
+        """
     )
 
-    # Рандомний вибір медалі
+
     def choose_medal():
         return random.choice(['calc_Bronze', 'calc_Silver', 'calc_Gold'])
 
@@ -49,40 +47,40 @@ with DAG(
         python_callable=choose_medal
     )
 
-    # Підрахунок та запис у таблицю для Bronze
-    calc_Bronze = SQLExecuteQueryOperator(
+
+    calc_Bronze = MySqlOperator(
         task_id='calc_Bronze',
-        conn_id='mysql_default',
+        mysql_conn_id='mysql_default',
         sql="""
             INSERT INTO neo_data.of_medals_results (medal_type, count)
-            SELECT 'Bronze', COUNT(*) FROM olympic_dataset.athlete_event_results WHERE medal = 'Bronze';
-        """,
-        hook_params={'schema': 'neo_data'}
+            SELECT 'Bronze', COUNT(*) FROM olympic_dataset.athlete_event_results
+            WHERE medal = 'Bronze';
+        """
     )
 
-    # Підрахунок та запис у таблицю для Silver
-    calc_Silver = SQLExecuteQueryOperator(
+
+    calc_Silver = MySqlOperator(
         task_id='calc_Silver',
-        conn_id='mysql_default',
+        mysql_conn_id='mysql_default',
         sql="""
             INSERT INTO neo_data.of_medals_results (medal_type, count)
-            SELECT 'Silver', COUNT(*) FROM olympic_dataset.athlete_event_results WHERE medal = 'Silver';
-        """,
-        hook_params={'schema': 'neo_data'}
+            SELECT 'Silver', COUNT(*) FROM olympic_dataset.athlete_event_results
+            WHERE medal = 'Silver';
+        """
     )
 
-    # Підрахунок та запис у таблицю для Gold
-    calc_Gold = SQLExecuteQueryOperator(
+
+    calc_Gold = MySqlOperator(
         task_id='calc_Gold',
-        conn_id='mysql_default',
+        mysql_conn_id='mysql_default',
         sql="""
             INSERT INTO neo_data.of_medals_results (medal_type, count)
-            SELECT 'Gold', COUNT(*) FROM olympic_dataset.athlete_event_results WHERE medal = 'Gold';
-        """,
-        hook_params={'schema': 'neo_data'}
+            SELECT 'Gold', COUNT(*) FROM olympic_dataset.athlete_event_results
+            WHERE medal = 'Gold';
+        """
     )
 
-    # Затримка 35 секунд
+
     def delay_task():
         time.sleep(35)
 
@@ -91,7 +89,7 @@ with DAG(
         python_callable=delay_task
     )
 
-    # Сенсор перевіряє, чи є новий запис не старший за 30 секунд
+
     check_for_correctness = SqlSensor(
         task_id='check_for_correctness',
         conn_id='mysql_default',
@@ -99,13 +97,11 @@ with DAG(
             SELECT COUNT(*) FROM neo_data.of_medals_results
             WHERE created_at >= NOW() - INTERVAL 30 SECOND;
         """,
-        poke_interval=10,
         timeout=60,
-        mode='poke',
-        hook_params={'schema': 'neo_data'}
+        poke_interval=10
     )
 
-    # Залежності
+
     create_table >> pick_medal >> pick_medal_task
     pick_medal_task >> [calc_Bronze, calc_Silver, calc_Gold]
     [calc_Bronze, calc_Silver, calc_Gold] >> generate_delay >> check_for_correctness
