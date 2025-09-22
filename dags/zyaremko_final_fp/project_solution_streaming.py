612291bac5a1813f761153a24738961b68d820b7
Zoryana Yaremko <zoryanaboyko@gmail.com>
2025-09-22 09:27:10 +0300

    Fix DAG for streaming pipeline

--- dags/zyaremko_final_fp/project_solution_streaming.py
+++ dags/zyaremko_final_fp/project_solution_streaming.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
@@ -7,20 +8,24 @@ default_args = {
     'start_date': datetime(2024, 8, 4, 0, 0),
 }
 
+DAG_ID = "project_solution_streaming"
+FOLDER = "zyaremko_final_fp"
+
 with DAG(
-        dag_id="project_solution_streaming",
-        default_args=default_args,
-        schedule_interval=None,
-        catchup=False,
-        tags=["zyaremko"]
+    dag_id=DAG_ID,
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False,
+    tags=["zyaremko"],
 ) as dag:
 
     streaming_task = SparkSubmitOperator(
         task_id="streaming_pipeline",
         conn_id="spark-default",
-        application="/opt/airflow/dags/zyaremko_final_fp/streaming_pipeline.py",
+        application=f"dags/{FOLDER}/streaming_pipeline.py",
         verbose=True,
     )
+
 
 
 
