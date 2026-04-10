49415b2c6905047f0947b1d97cabb91f2d5d742e
Darymee <danieledaniele3005@gmail.com>
2026-04-10 19:27:48 +0200

    add dag

--- daryna_lysenko_dags.py
+++ daryna_lysenko_dags.py
@@ -4,13 +4,14 @@ import time
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator, BranchPythonOperator
-from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.common.sql.sensors.sql import SqlSensor
 from airflow.utils.trigger_rule import TriggerRule
 
 
 DB_CONN = "DBneodata"
-RESULT_TABLE = "daryna_lysenko.medal_statistics"
+DB_SCHEMA = "daryna_lysenko"
+RESULT_TABLE = f"{DB_SCHEMA}.medal_statistics"
 
 
 def get_random_medal():
@@ -21,13 +22,11 @@ def get_random_medal():
 
 def choose_branch(**kwargs):
     medal_value = kwargs["ti"].xcom_pull(task_ids="select_medal")
-
     mapping = {
         "Gold": "save_gold_count",
         "Silver": "save_silver_count",
         "Bronze": "save_bronze_count",
     }
-
     return mapping[medal_value]
 
 
@@ -44,9 +43,10 @@ with DAG(
     tags=["airflow", "mysql", "homework"],
 ) as dag:
 
-    init_table = MySqlOperator(
+    init_table = SQLExecuteQueryOperator(
         task_id="init_table",
-        mysql_conn_id=DB_CONN,
+        conn_id=DB_CONN,
+        hook_params={"schema": DB_SCHEMA},
         sql=f"""
         CREATE TABLE IF NOT EXISTS {RESULT_TABLE} (
             id INT NOT NULL AUTO_INCREMENT,
@@ -68,9 +68,10 @@ with DAG(
         python_callable=choose_branch,
     )
 
-    save_gold_count = MySqlOperator(
+    save_gold_count = SQLExecuteQueryOperator(
         task_id="save_gold_count",
-        mysql_conn_id=DB_CONN,
+        conn_id=DB_CONN,
+        hook_params={"schema": DB_SCHEMA},
         sql=f"""
         INSERT INTO {RESULT_TABLE} (medal_type, medal_count, created_at)
         SELECT 'Gold', COUNT(*), NOW()
@@ -79,9 +80,10 @@ with DAG(
         """,
     )
 
-    save_silver_count = MySqlOperator(
+    save_silver_count = SQLExecuteQueryOperator(
         task_id="save_silver_count",
-        mysql_conn_id=DB_CONN,
+        conn_id=DB_CONN,
+        hook_params={"schema": DB_SCHEMA},
         sql=f"""
         INSERT INTO {RESULT_TABLE} (medal_type, medal_count, created_at)
         SELECT 'Silver', COUNT(*), NOW()
@@ -90,9 +92,10 @@ with DAG(
         """,
     )
 
-    save_bronze_count = MySqlOperator(
+    save_bronze_count = SQLExecuteQueryOperator(
         task_id="save_bronze_count",
-        mysql_conn_id=DB_CONN,
+        conn_id=DB_CONN,
+        hook_params={"schema": DB_SCHEMA},
         sql=f"""
         INSERT INTO {RESULT_TABLE} (medal_type, medal_count, created_at)
         SELECT 'Bronze', COUNT(*), NOW()
@@ -110,6 +113,7 @@ with DAG(
     recent_record_sensor = SqlSensor(
         task_id="recent_record_sensor",
         conn_id=DB_CONN,
+        hook_params={"schema": DB_SCHEMA},
         sql=f"""
         SELECT IF(
             MAX(created_at) > NOW() - INTERVAL 30 SECOND,
