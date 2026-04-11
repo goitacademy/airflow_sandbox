96cbc8945a751710b4d6e27caca7ebead3d3f6a4
Anatolii <anatoliivorobiov1@gmail.com>
2026-04-11 01:29:37 +0000

    fix: properly extract dag file after decryption

--- decrypt_and_extract.sh
+++ decrypt_and_extract.sh
@@ -8,7 +8,7 @@ fi
 
 ENCRYPTED_ARCHIVE="$1"
 PRIVATE_KEY="$2"
-DECRYPTED_FILE="decrypted_$(basename "$ENCRYPTED_ARCHIVE" _encrypted_parts.tar.gz)"
+ORIGINAL_NAME=$(basename "$ENCRYPTED_ARCHIVE" _encrypted_parts.tar.gz)
 
 echo "Розпаковуємо архів $ENCRYPTED_ARCHIVE..."
 tar -xzf "$ENCRYPTED_ARCHIVE"
@@ -21,19 +21,15 @@ for enc_part in temp_parts/*.enc; do
     openssl rsautl -decrypt -inkey "$PRIVATE_KEY" -in "$enc_part" -out "$dec_part"
 done
 
-echo "Об'єднуємо частини у архів reconstructed_archive.tar.gz..."
-cat $(ls temp_parts/part_* | grep -v '\.enc$' | sort) > reconstructed_archive.tar.gz
-echo "=== Размер reconstructed_archive.tar.gz ==="
-ls -lh reconstructed_archive.tar.gz
-file reconstructed_archive.tar.gz
+echo "Об'єднуємо частини..."
+cat $(ls temp_parts/part_* | grep -v '\.enc$' | sort) > temp_restored.tar.gz
 
-echo "Перевіряємо що отримали..."
-file reconstructed_archive.tar.gz
+echo "Розпаковуємо відновлений архів..."
+tar -xzf temp_restored.tar.gz
 
-echo "Файл успішно відновлено!"
-cp reconstructed_archive.tar.gz "$DECRYPTED_FILE"
+echo "Файл успішно відновлено: $ORIGINAL_NAME"
 
 # Очистка
-rm -rf temp_parts/ reconstructed_archive.tar.gz
+rm -rf temp_parts/ temp_restored.tar.gz
 
-echo "Дешифрований файл: $DECRYPTED_FILE"
+echo "Дешифрований файл: $ORIGINAL_NAME"
