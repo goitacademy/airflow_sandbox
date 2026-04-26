96dd26e84eef4461fc303f2ef42854506fc7af0b
Sergiy Pavlyshyn <pavlyshynserjiy@gmail.com>
2026-04-26 17:42:08 +0300

    Renovate path

--- dags/sergiy_pavlyshyn_fp/project_solution.py
+++ dags/sergiy_pavlyshyn_fp/project_solution.py
@@ -1,3 +1,4 @@
+import os
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 import pendulum
@@ -8,31 +9,31 @@ default_args = {
     "catchup": False,
 }
 
+DAG_FOLDER = os.path.dirname(os.path.realpath(__file__))
+
 with DAG(
     dag_id="sergiy_pavlyshyn_datalake_pipeline",
     default_args=default_args,
     schedule_interval=None,
     tags=["sergiy_pavlyshyn", "final_project"],
 ) as dag:
-    BASE_PATH = "dags/sergiy_pavlyshyn_fp"
-
     task_landing_to_bronze = SparkSubmitOperator(
         task_id="run_landing_to_bronze",
-        application=f"{BASE_PATH}/landing_to_bronze.py",
+        application=f"{DAG_FOLDER}/landing_to_bronze.py",  # Тепер шлях завжди буде точним
         conn_id="spark-default",
         verbose=1,
     )
 
     task_bronze_to_silver = SparkSubmitOperator(
         task_id="run_bronze_to_silver",
-        application=f"{BASE_PATH}/bronze_to_silver.py",
+        application=f"{DAG_FOLDER}/bronze_to_silver.py",
         conn_id="spark-default",
         verbose=1,
     )
 
     task_silver_to_gold = SparkSubmitOperator(
         task_id="run_silver_to_gold",
-        application=f"{BASE_PATH}/silver_to_gold.py",
+        application=f"{DAG_FOLDER}/silver_to_gold.py",
         conn_id="spark-default",
         verbose=1,
     )
