87cc26ac1a910189c2931ae02ad72cd76b8d60fc
RomanS <siusiailoroman@gmail.com>
2026-04-27 07:29:24 +0000

    update romans fin_dag

--- dags/rfp/project_solution.py
+++ dags/rfp/project_solution.py
@@ -4,44 +4,57 @@ from datetime import datetime
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-# -------------------- paths --------------------
-DAG_DIR = os.path.dirname(os.path.abspath(__file__))
-# ------------------------------------------------
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+LANDING_TO_BRONZE_APP = os.path.join(BASE_DIR, "landing_to_bronze.py")
+BRONZE_TO_SILVER_APP = os.path.join(BASE_DIR, "bronze_to_silver.py")
+SILVER_TO_GOLD_APP = os.path.join(BASE_DIR, "silver_to_gold.py")
 
 
 default_args = {
-    'owner': 'airflow',
-    'start_date': datetime(2024, 8, 4, 0, 0),
+    "owner": "airflow",
+    "start_date": datetime(2026, 4, 1),
 }
 
 with DAG(
-        'romans_fin_proj_dag',
-        default_args=default_args,
-        schedule_interval=None,
-        catchup=False,
-        tags=["romans"]
+    dag_id="romans_fin_proj_dag",
+    default_args=default_args,
+    schedule=None,
+    catchup=False,
+    max_active_runs=1,
+    tags=["romans"],
 ) as dag:
-
-    landing_to_bronze_task = SparkSubmitOperator(
-        task_id="run_landing_to_bronze",
-        application=f"{DAG_DIR}/landing_to_bronze.py",
+    landing_to_bronze = SparkSubmitOperator(
+        task_id="landing_to_bronze",
+        application=LANDING_TO_BRONZE_APP,
         conn_id="spark-default",
-        verbose=1,
+        driver_memory="512m",
+        executor_memory="512m",
+        executor_cores=1,
+        total_executor_cores=1,
+        verbose=True,
     )
 
-    bronze_to_silver_task = SparkSubmitOperator(
-        task_id="run_bronze_to_silver",
-        application=f"{DAG_DIR}/bronze_to_silver.py",
+    bronze_to_silver = SparkSubmitOperator(
+        task_id="bronze_to_silver",
+        application=BRONZE_TO_SILVER_APP,
         conn_id="spark-default",
-        verbose=1,
+        driver_memory="512m",
+        executor_memory="512m",
+        executor_cores=1,
+        total_executor_cores=1,
+        verbose=True,
     )
 
-    silver_to_gold_task = SparkSubmitOperator(
-        task_id="run_silver_to_gold",
-        application=f"{DAG_DIR}/silver_to_gold.py",
+    silver_to_gold = SparkSubmitOperator(
+        task_id="silver_to_gold",
+        application=SILVER_TO_GOLD_APP,
         conn_id="spark-default",
-        verbose=1,
+        driver_memory="512m",
+        executor_memory="512m",
+        executor_cores=1,
+        total_executor_cores=1,
+        verbose=True,
     )
 
-    landing_to_bronze_task >> bronze_to_silver_task
-    bronze_to_silver_task >> silver_to_gold_task
+    landing_to_bronze >> bronze_to_silver >> silver_to_gold
