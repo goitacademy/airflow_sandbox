2f991c6d2aeb0655b0f02e4c073fbe3321ebf4ce
Kostya Vasulyak <vasulyak90@gmail.com>
2026-05-01 21:10:28 +0300

    bash_operator

--- dags/fp_ds_kos/fp_2_project_solution.py
+++ dags/fp_ds_kos/fp_2_project_solution.py
@@ -1,53 +1,34 @@
 from airflow import DAG
-from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.bash import BashOperator
 
 import os
 from datetime import datetime
 
-default_args = {
-    'owner':'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 10, 1),
-}
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-landing_script = os.path.join(current_directory, "fp_2_landing_to_bronze.py")
-bronze_script = os.path.join(current_directory, "fp_2_bronze_to_silver.py")
-gold_script = os.path.join(current_directory, "fp_2_silver_to_gold.py")
-
+DAG_DIR = os.path.dirname(os.path.abspath(__file__))
 
 with DAG(
-    dag_id='fp_2_project_solution_ds',
-    default_args=default_args,
-    description='Landing -> Bronze -> Silver -> Gold',
-    schedule=None,
-    tags=['kostiya'],
+    dag_id="fp_2_project_solution_ds",
+    start_date=datetime(2024, 10, 1),
+    schedule_interval=None,
     catchup=False,
-)as dag:
+    max_active_runs=1,
+    max_active_tasks=1,
+    tags=["fp_2_project_solution"],
+) as dag:
 
-    landing_to_bronze = SparkSubmitOperator(
-        application=os.path.join(current_directory, "fp_2_landing_to_bronze.py"),
+    landing_to_bronze = BashOperator(
         task_id="landing_to_bronze",
-        conn_id="spark_default",
-        py_files=os.path.join(current_directory, "downloader.py"),
-        verbose=True,
+        bash_command=f"spark-submit {os.path.join(DAG_DIR, 'fp_2_landing_to_bronze.py')}",
     )
 
-    bronze_to_silver = SparkSubmitOperator(
-        application=os.path.join(current_directory, "fp_2_bronze_to_silver.py"),
+    bronze_to_silver = BashOperator(
         task_id="bronze_to_silver",
-        conn_id="spark_default",
-        verbose=True,
+        bash_command=f"spark-submit {os.path.join(DAG_DIR, 'fp_2_bronze_to_silver.py')}",
     )
 
-    silver_to_gold = SparkSubmitOperator(
-        application=os.path.join(current_directory, "fp_2_silver_to_gold.py"),
+    silver_to_gold = BashOperator(
         task_id="silver_to_gold",
-        conn_id="spark_default",
-        verbose=True,
+        bash_command=f"spark-submit {os.path.join(DAG_DIR, 'fp_2_silver_to_gold.py')}",
     )
 
     landing_to_bronze >> bronze_to_silver >> silver_to_gold
-
-
