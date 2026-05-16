071fc9dc32c9468cd6b08fe230b363bbe910657d
nata87 <nata_pustelnyk@ukr.net>
2026-05-16 19:04:35 +0200

    Use dynamic paths for SparkSubmit

--- dags/nata-goit-de-fp/project_solution.py
+++ dags/nata-goit-de-fp/project_solution.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
@@ -9,6 +10,11 @@ default_args = {
     "retries": 1,
 }
 
+# Динамічно визначаємо шлях до папки dags на сервері
+# Зазвичай це /opt/airflow/dags/назва_папки/скрипт.py
+airflow_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow")
+base_path = os.path.join(airflow_home, "dags", "nata-goit-de-fp")
+
 with DAG(
     dag_id="nata-goit-de-hw-final-project",
     default_args=default_args,
@@ -19,21 +25,23 @@ with DAG(
 
     landing_to_bronze = SparkSubmitOperator(
         task_id="nata_landing_to_bronze",
-        application="nata-goit-de-fp/landing_to_bronze.py",  
+        application=os.path.join(base_path, "landing_to_bronze.py"),
+        conn_id="spark-default",
         verbose=True,
     )
 
     bronze_to_silver = SparkSubmitOperator(
         task_id="nata_bronze_to_silver",
-        application="nata-goit-de-fp/bronze_to_silver.py",   
+        application=os.path.join(base_path, "bronze_to_silver.py"),
         conn_id="spark-default",
         verbose=True,
     )
 
     silver_to_gold = SparkSubmitOperator(
         task_id="nata_silver_to_gold",
-        application="nata-goit-de-fp/silver_to_gold.py",     
+        application=os.path.join(base_path, "silver_to_gold.py"),
         conn_id="spark-default",
         verbose=True,
     )
+
     landing_to_bronze >> bronze_to_silver >> silver_to_gold
